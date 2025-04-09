ddf19d0c2af9556c51cc717926beb883081992cb
kPherox <kphrx@kpherox.dev>
2025-04-10 01:03:11 +0900

    fix: incorrect escape

--- src/_pocket.js
+++ src/_pocket.js
@@ -481,9 +481,9 @@ AntiPrototypeJs().then(() => {
       .nicoadVideoItemWrapper {
         display: none;
       }
-      div:has(> div > div > div > div > a[data-anchor-page="ranking_for-you"] > div > div:is(.c_serviceColor\.nicoadGold, .c_serviceColor\.nicoadGray)),
-      a[data-anchor-page="ranking_genre"]:has(> div > div:is(.c_serviceColor\.nicoadGold, .c_serviceColor\.nicoadGray)),
-      div:has(> a[data-anchor-page="ranking_custom"] > div > div:is(.c_serviceColor\.nicoadGold, .c_serviceColor\.nicoadGray)) {
+      div:has(> div > div > div > div > a[data-anchor-page="ranking_for-you"] > div > div:is(.c_serviceColor\\.nicoadGold, .c_serviceColor\\.nicoadGray)),
+      a[data-anchor-page="ranking_genre"]:has(> div > div:is(.c_serviceColor\\.nicoadGold, .c_serviceColor\\.nicoadGray)),
+      div:has(> a[data-anchor-page="ranking_custom"] > div > div:is(.c_serviceColor\\.nicoadGold, .c_serviceColor\\.nicoadGray)) {
         display: none;
       }
     `.trim();
